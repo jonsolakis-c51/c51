2760897946b12274b77af308b2a7ccf348b43330
Jon Solakis <jonsolakis@gmail.com>
2020-10-19 09:30:37 -0400

    add database migration and seeder

---
+++ database/migrations/2020_10_18_182202_create_offers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('offer_id');
+            $table->string('name');
+            $table->string('image_url');
+            $table->decimal('cash_back');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
